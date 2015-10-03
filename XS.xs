ca148dcf6d8a8128c2e3c087ab64dbeb6386c901
Davs <kovdavid@gmail.com>
2015-10-03 21:15:52 +0200

    Initial XS library test run

--- XS.xs
+++ XS.xs
@@ -1,10 +1,18 @@
-#define PERL_NO_GET_CONTEXT
 #include "EXTERN.h"
 #include "perl.h"
 #include "XSUB.h"
 
-#include "ppport.h"
+#include "perl_math_int64.h"
 
+MODULE = Data::AMQP::XS	PACKAGE = Data::AMQP::XS PREFIX = data_amqp_xs_
 
-MODULE = Data::AMQP::XS		PACKAGE = Data::AMQP::XS		
+REQUIRE:        1.9505
+PROTOTYPES:     DISABLE
 
+BOOT:
+    PERL_MATH_INT64_LOAD_OR_CROAK;
+
+void
+data_amqp_xs_test()
+    CODE:
+        printf("hello world\n");
--- lib/Data/AMQP/XS.pm
+++ lib/Data/AMQP/XS.pm
@@ -1,86 +1,12 @@
 package Data::AMQP::XS;
 
-use 5.020001;
+use v5.10;
 use strict;
 use warnings;
-
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Data::AMQP::XS ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
 
 our $VERSION = '0.01';
 
 require XSLoader;
 XSLoader::load('Data::AMQP::XS', $VERSION);
 
-# Preloaded methods go here.
-
 1;
-__END__
-# Below is stub documentation for your module. You'd better edit it!
-
-=head1 NAME
-
-Data::AMQP::XS - Perl extension for blah blah blah
-
-=head1 SYNOPSIS
-
-  use Data::AMQP::XS;
-  blah blah blah
-
-=head1 DESCRIPTION
-
-Stub documentation for Data::AMQP::XS, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
-=head2 EXPORT
-
-None by default.
-
-
-
-=head1 SEE ALSO
-
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
-
-=head1 AUTHOR
-
-A. U. Thor, E<lt>davs@nonetE<gt>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright (C) 2015 by A. U. Thor
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.20.1 or,
-at your option, any later version of Perl 5 you may have available.
-
-
-=cut
